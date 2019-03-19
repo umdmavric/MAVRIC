a753e615ba68ff9001fadab30fea59c9ff733464
Japneet Singh <jpntsingh8@gmail.com>
2019-03-19 17:13:48 -0500

    added sections

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 let head = document.querySelector('.head');
 head.style.height = screen.height/2;
 console.log(screen.height)
+
+let content = document.querySelectorAll('.who');
+console.log(content)
+
+let check = true;
+
+content.forEach((elm) => {
+    elm.addEventListener("click",()=>{
+        let hidden = (elm.childNodes[5]);
+        if (check) {
+            hidden.style.display = "block";
+            check = false;
+        }    
+        else {
+            hidden.style.display = "none";
+            check = true;
+        }      
+    })
+})

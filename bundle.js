259645b842bdd71e877ba65ff35ea87018f4f477
japsuchiha <jpntsingh8@gmail.com>
2019-03-31 01:58:10 -0400

    mobile nav

--- bundle.js
+++ bundle.js
@@ -205,20 +205,32 @@ for (let i = 0; i < nav.children.length; i++) {
 
 let nav2 = document.querySelector('.mob_links');
 console.log(nav2.children);
-
-for (let i = 0; i < nav2.children.length; i++) {
-  nav2.children[i].addEventListener("click", () => {
-    console.log(nav2.children[i].innerHTML);
-    if (nav2.children[i].innerHTML == "About") (0, _jump.default)('.about');
-    if (nav2.children[i].innerHTML == "Prices") (0, _jump.default)('.prices');
-  });
-}
-
 let hamburger = document.querySelector('.hamburger');
 console.log(hamburger);
 let hcheck = true;
 let nav_mob = document.querySelector('.small');
 console.log(nav_mob);
+
+for (let i = 0; i < nav2.children.length; i++) {
+  nav2.children[i].addEventListener("click", () => {
+    console.log(nav2.children[i].innerHTML);
+
+    if (nav2.children[i].innerHTML == "About") {
+      hamburger.className = "hamburger hamburger--spin";
+      nav_mob.className += "small";
+      (0, _jump.default)('.about');
+      hcheck = true;
+    }
+
+    if (nav2.children[i].innerHTML == "Prices") {
+      hamburger.className = "hamburger hamburger--spin";
+      nav_mob.className += "small";
+      (0, _jump.default)('.prices');
+      hcheck = true;
+    }
+  });
+}
+
 hamburger.addEventListener("click", () => {
   if (hcheck) {
     hamburger.className += " is-active";
--- src/script.js
+++ src/script.js
@@ -38,21 +38,31 @@ for (let i = 0;i<nav.children.length;i++) {
 
 let nav2 = document.querySelector('.mob_links');
 console.log(nav2.children);
-for (let i = 0;i<nav2.children.length;i++) {
-    nav2.children[i].addEventListener("click",()=>{
-        console.log(nav2.children[i].innerHTML)
-        if(nav2.children[i].innerHTML == "About")
-            jump('.about')
-        if(nav2.children[i].innerHTML == "Prices")
-            jump('.prices')
-    })
-}
-
 let hamburger = document.querySelector('.hamburger');
 console.log(hamburger)
 let hcheck = true;
 let nav_mob = document.querySelector('.small');
 console.log(nav_mob);
+
+for (let i = 0;i<nav2.children.length;i++) {
+    nav2.children[i].addEventListener("click",()=>{
+        console.log(nav2.children[i].innerHTML)
+        if(nav2.children[i].innerHTML == "About") {
+            hamburger.className = "hamburger hamburger--spin"
+            nav_mob.className += "small"
+            jump('.about')
+            hcheck = true
+        }
+        if(nav2.children[i].innerHTML == "Prices") {
+            hamburger.className = "hamburger hamburger--spin"
+            nav_mob.className += "small"
+            jump('.prices')
+            hcheck = true
+        }
+    })
+}
+
+
 
 hamburger.addEventListener("click",()=>{
     if(hcheck) {

cfc9df670ed2872b0a15052dce4f73ef17f24a0d
Japneet Singh <jpntsingh8@gmail.com>
2019-08-09 15:07:31 -0400

    mobile speakers

--- bundle2.js
+++ bundle2.js
@@ -1,52 +1,54 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
-// let speaker = (name,work,img) => {
-//     let speaker_template = `
-//     <li class="glide__slide">
-//                                 <div class="speaker-card">
-//                                         <img src=${img} style="border-top-left-radius: 5px; border-top-right-radius: 5px;">
-//                                         <p id="name">${name}</p>
-//                                         <p id="work">${work}</p>
-//                                 </div>
-//                             </li>
-//     `
-//     let prev_html = document.querySelector('.glide__slides').innerHTML
-//     document.querySelector('.glide__slides').innerHTML = prev_html+speaker_template
-// }
+module.exports = {
+    "apikey" : "keyhyniEmyt4kcvvs"
+}
+},{}],2:[function(require,module,exports){
+"use strict";
 
-// console.log(document.querySelector('.glide__slides'))
+var _apiKey = _interopRequireDefault(require("./apiKey"));
 
-// // let cards = document.querySelectorAll('.speaker-card')
-// // for(let i = 0;i<cards.length;i++) {
-// //     cards[i].addEventListener('click',(e) => {
-// //     let name = cards[i].childNodes[3].innerHTML
-// //     let work = cards[i].childNodes[5].innerHTML
-// //     fetch(url)
-// //     .then((resp) => {
-// //         resp.json().then((data) => {
-// //             console.log(data)
-// //             data.records.forEach(element => {
-// //                 console.log(element.fields.Name)
-// //                 console.log(element.fields.Company)
-// //                 if(element.fields.Headshot && element.fields.Name == name) {
-// //                     console.log(element.fields.Headshot[0].url)
-// //                     let opened = window.open("");
-// //                     opened.document.write(`<html>
-// //                     <head>
-// //                     <link rel="stylesheet" href="https://unpkg.com/chota@latest">
-// //                     <style>
-// //                     body {background-color:black}
-// //                     </style>
-// //                     <title>MyTitle</title></head><body style="color:blue">test</body></html>`);
-// //                     // speaker(element.fields.Name, element.fields.Company,element.fields.Headshot[0].url)
-// //                 }
-                    
-// //             });
-// //         })
-// //     })  
-// //     })
-// // }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+let mobile_display = `
+    <div class = "row">
+        <div class = "col left-side">
+        </div>
+    </div>
+`;
+let url = `https://api.airtable.com/v0/appiwCrmeV2rljaOH/Speakers?api_key=${_apiKey.default.apikey}`;
+console.log(document.querySelector('.head'));
 
+let mobile = width => {
+  let i = 0;
+  let carousel = document.querySelector('.carousel');
+  console.log(carousel);
 
-},{}]},{},[1]);
+  if (width.matches) {
+    carousel.innerHTML = mobile_display;
+    let left = document.querySelector('.left-side');
+    let right = document.querySelector('.right-side');
+    fetch(url).then(resp => {
+      resp.json().then(data => {
+        data.records.forEach(element => {
+          if (element.fields.Headshot) {
+            let insert = `
+                                <div class = "speaker-circ">
+                                    <img src = "${element.fields.Headshot[0].url}">
+                                    <p id="name_sm" style="font-weight:bold">${element.fields.Name}</p>
+                                    <p id="work_sm">${element.fields.Company}</p>
+                                </div>
+                            `;
+            left.innerHTML += insert;
+          }
+        });
+      });
+    });
+  }
+};
+
+let x = window.matchMedia("(max-width:768px)");
+mobile(x);
+x.addListener(mobile);
+
+},{"./apiKey":1}]},{},[2]);
---
+++ src/generate.js
@@ -0,0 +1,42 @@
+import apiConfig from './apiKey'
+let mobile_display = 
+`
+    <div class = "row">
+        <div class = "col left-side">
+        </div>
+    </div>
+`
+let url = `https://api.airtable.com/v0/appiwCrmeV2rljaOH/Speakers?api_key=${apiConfig.apikey}`
+
+console.log(document.querySelector('.head'))
+let mobile  = (width) => {
+    let i = 0
+    let carousel = document.querySelector('.carousel')
+    console.log(carousel)
+    if(width.matches) {
+        carousel.innerHTML = mobile_display
+        let left = document.querySelector('.left-side')
+        let right = document.querySelector('.right-side')
+        fetch(url)
+        .then((resp) => {
+            resp.json().then((data) => {
+                data.records.forEach(element => {
+                    if(element.fields.Headshot) {
+                            let insert = 
+                            `
+                                <div class = "speaker-circ">
+                                    <img src = "${element.fields.Headshot[0].url}">
+                                    <p id="name_sm" style="font-weight:bold">${element.fields.Name}</p>
+                                    <p id="work_sm">${element.fields.Company}</p>
+                                </div>
+                            `
+                            left.innerHTML += insert
+                    }
+                })
+            })
+        })
+    }
+}
+let x = window.matchMedia("(max-width:768px)")
+mobile(x)
+x.addListener(mobile)
